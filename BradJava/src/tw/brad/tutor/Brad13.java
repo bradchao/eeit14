64bf2c8c47dc6086c8cdd8743718b58f3e5a08f0
mickeyyo55b <mickeyyo55b@gmail.com>
2026-01-05 10:45:45 +0800

    01051045

--- BradJava/src/tw/brad/tutor/Brad13.java
+++ BradJava/src/tw/brad/tutor/Brad13.java
@@ -5,10 +5,10 @@ public class Brad13 {
 	public static void main(String[] args) {
 		int[] p = new int[7];
 		
-		for (int i=0; i<100 ; i++) {
-			int point = (int)(Math.random()* 6)  + 1;	// 1 ~ 6
-			if (point >= 1 && point <= 6) {
-				p[point]++;
+		for (int i=0; i<1000000 ; i++) {
+			int point = (int)(Math.random()* 9)  + 1;	// 1 ~ 9
+			if (point >= 1 && point <= 9) {
+				p[point>=7?point-3:point]++;
 			}else {
 				p[0]++;
 			}
