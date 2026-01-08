50a513370f943e5bab7343ee4b41c67fb23127dd
mickeyyo55b <mickeyyo55b@gmail.com>
2026-01-08 10:08:48 +0800

    01081008

--- BradJava/src/tw/brad/apis/MyDrawer.java
+++ BradJava/src/tw/brad/apis/MyDrawer.java
@@ -1,7 +1,28 @@
 package tw.brad.apis;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel {
-
+	public MyDrawer() {
+		setBackground(Color.YELLOW);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setStroke(new BasicStroke(4));
+		g2d.setColor(Color.BLUE);
+		g2d.drawLine(0, 0, 200, 300);
+		
+	}
+	
 }
