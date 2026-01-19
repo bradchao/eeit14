1ec2252a188e11b73b8035028b6100b39f3335e7
mickeyyo55b <mickeyyo55b@gmail.com>
2026-01-19 10:11:49 +0800

    01191011

---
+++ JDBC/src/tw/brad/tutor/Brad13.java
@@ -0,0 +1,31 @@
+package tw.brad.tutor;
+
+import java.util.List;
+
+import tw.brad.apis.Gift;
+import tw.brad.apis.JdbcTool;
+import tw.brad.apis.Member;
+
+public class Brad13 {
+
+	public static void main(String[] args) {
+		JdbcTool tool = new JdbcTool();
+		String sql = """
+				SELECT email, name 
+				FROM member
+				""";
+		
+		List<Member> gifts = tool.query(sql, rs -> {
+			Member member = new Member();
+			member.setName(rs.getString("name"));
+			member.setEmail(rs.getString("email"));
+			return member;
+		});
+		
+		
+		gifts.forEach(System.out::println);
+		
+		
+	}
+
+}
